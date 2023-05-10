203b9778fe3f5c75e7d688cff39b0b3a180cbf95
ButterflyLion <orosztama@gmail.com>
2023-05-10 23:18:13 +0100

    form added from template

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,7 @@
 from splinter import Browser
+# https://splinter.readthedocs.io/en/latest/api/driver-and-element-api.html
+# https://splinter.readthedocs.io/en/latest/tutorial.html
+
 browser = Browser('chrome')
 url = 'http://localhost:5000'
 browser.visit(url)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
 from flask import Flask , session, redirect, url_for, request, render_template
-import oracledb
+#import oracledb
 
 # Replace with your actual Oracle database credentials
 user = 'SYS'
@@ -9,14 +9,13 @@ port = 1522
 service_name = 'XEPDB1'
 conn_string = "localhost:{port}/{service_name}".format(
     port=port, service_name=service_name)
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
 data = []
 id = []
 
 @app.route('/')
-def home():
-    return 'hello world'
-    #return render_template('home.html')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
