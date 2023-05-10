aef3fc189a885dd70dc94755b6efb9bf93e2a32c
ButterflyLion <orosztama@gmail.com>
2023-05-10 17:32:53 +0100

    updated functional_test

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,11 @@
 from splinter import Browser
+from selenium.webdriver.chrome.service import Service
 
-browser = Browser()
+my_service = Service()
+browser = Browser('chrome', service=my_service)
+
 url = 'http://localhost'
 browser.visit(url)
 assert browser.is_text_present('hello world')
 browser.quit()
+
