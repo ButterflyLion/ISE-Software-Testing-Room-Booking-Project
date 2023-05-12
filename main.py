c06f4c0c4c96d16f19d68a424c3fbfbbc087e7d0
conorglynn02 <conorglynn02@gmail.com>
2023-05-12 14:46:01 +0100

    Added basic login and homepage working with flask

--- main.py
+++ main.py
@@ -13,10 +13,17 @@ app = Flask(__name__, template_folder='templateFiles', static_folder='staticFile
 data = []
 id = []
 
-@app.route('/')
+@app.route('/homepage')
+def home():
+    return render_template('homepage.html')
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
+@app.route('/')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
