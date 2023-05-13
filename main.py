37af9a456beff02fd925f8b5c5f3b084c629792f
pardisnorouzi <22223614@studentmail.ul.ie>
2023-05-13 18:22:12 +0100

    users can login with pass 12345, Welcome Working Room showing the allowed rooms for the current user and also based on number of guests now working on book button and to show dates

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import oracledb
 import hashlib
 
 # Replace with your actual Oracle database credentials
-user = 'SYS'
+user = 'SYSTEM'
 password = 'root'
-port = 1522
+port = 1521
 service_name = 'XEPDB1'
 conn_string = "localhost:{port}/{service_name}".format(
     port=port, service_name=service_name)
@@ -22,11 +22,78 @@ id = []
 def home():
     return render_template('homepage.html')
 
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+
+    if 'username' not in session:
+        # User is not logged in, redirect to login page
+        return redirect(url_for('login'))
+    else:
+
+        err = False
+        data = []
+
+
+        con = oracledb.connect(user=user, password=password, dsn=conn_string)
+        cur = con.cursor()
+        
+        stage = request.form.get('s', 'main') #is s is posted, set stage to that otherwise set it to main.
+        print(stage) 
+        print(request.form)
+
+        if stage == 'main':
+            session['BGuets'] = ''
+            session['BRoom'] = ''
+            session['BDate'] = ''
+            session['BHour'] = ''
+
+
+
+
+
+        if request.method == 'POST':
+
+            if stage == 'rooms':
+                NoGuests = request.form.get('guests', '0')
+                print(NoGuests)
+                cur.execute("SELECT room_type_id FROM RBS.RoomUserRole WHERE user_role_id = :userroleid", {'userroleid': session['userroleid']})
+                RoomTypesAllowed = [row[0] for row in cur.fetchall()] #getting Rooms allowed for this user
+                RoomTypesAllowedString = format(','.join(str(i) for i in RoomTypesAllowed));
+                print(RoomTypesAllowedString)
+
+                query = "SELECT ROOM.*, ROOMTYPE.r_type FROM RBS.ROOM \
+                        LEFT JOIN RBS.ROOMTYPE ON RBS.ROOM.room_type = RBS.ROOMTYPE.room_type_id \
+                        WHERE ROOM.room_type IN ({}) AND ROOM.mincapacity <= :NoGuests AND ROOM.maxcapacity >= :NoGuests".format(','.join([':{}'.format(i+1) for i in range(len(RoomTypesAllowed))]))
+                params = {'NoGuests': NoGuests}
+                params.update({str(i+1): val for i, val in enumerate(RoomTypesAllowed)})
+                cur.execute(query, params)
+                for row in cur:
+                    data.append({"description": row[4], "min": row[2],
+                    "max": row[3]})
+       
+                
+
+                print('post is rooms')
+            elif stage == 'dates':
+                print('post is dates')
+            elif stage == 'hours':
+                print('post is hours')
+            elif stage == 'confirm':
+                print('post is guest')
+
+
+        # Close the cursor and connection
+        cur.close()
+        con.close()
+        return render_template('index.html',stage=stage,data=data)
+        
 
 @app.route('/', methods=['GET', 'POST'])
+def main():
+    return redirect(url_for('index'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
 
     if 'username' in session:
@@ -34,7 +101,6 @@ def login():
         return redirect(url_for('index'))
 
     err = False
-
     if request.method == 'POST':
         username = request.form['username']
         passh = hashlib.md5(request.form['password'].encode()).hexdigest()
@@ -43,16 +109,21 @@ def login():
         con = oracledb.connect(user=user, password=password, dsn=conn_string)
 
         with con.cursor() as cursor:
-            cursor.execute("SELECT * FROM RBS.USERS \
-                           WHERE LOWER(RBS.USER.email) = LOWER(:email)", {"email": username})
+            cursor.execute("SELECT RBS.USERS.*, RBS.ROLE.u_role FROM RBS.USERS \
+                           LEFT JOIN RBS.Role ON RBS.USERS.role_id = RBS.Role.role_id \
+                           WHERE LOWER(RBS.USERS.email) = LOWER(:email)", {"email": username})
             result = cursor.fetchone()
+            print(result)
 
             if result:
-                if passh == result[0]:
+                if passh == result[4]:
                     # Username and password are valid, so set user as logged in
                     session['username'] = username
-                    session['userrole'] = result[5]
-                    session['userid'] = result[1]
+                    session['firstname'] = result[1]
+                    session['lastname'] = result[2]
+                    session['userrole'] = result[7]
+                    session['userroleid'] = result[6]
+                    session['userid'] = result[0]
                     return redirect(url_for('index'))
                 else:
                     err = 'Invalid username or password'
