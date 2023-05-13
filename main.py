c179ab239f206c4d9e29aaba89a27e562a4b47ff
pardisnorouzi <22223614@studentmail.ul.ie>
2023-05-13 12:12:38 +0100

    adding login mechanism with a hashed password

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ service_name = 'XEPDB1'
 conn_string = "localhost:{port}/{service_name}".format(
     port=port, service_name=service_name)
 app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
+
+# This is for login Sessions
+app.secret_key = 'RBS-Secret' 
+
 data = []
 id = []
 
@@ -22,9 +26,42 @@ def home():
 def index():
     return render_template('index.html')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+
+    if 'username' in session:
+        # User is already logged in, redirect to index page
+        return redirect(url_for('index'))
+
+    err = False
+
+    if request.method == 'POST':
+        username = request.form['username']
+        passh = hashlib.md5(request.form['password'].encode()).hexdigest()
+        
+        #Connecting to database
+        con = oracledb.connect(user=user, password=password, dsn=conn_string)
+
+        with con.cursor() as cursor:
+            cursor.execute("SELECT * FROM RBS.USERS \
+                           WHERE LOWER(RBS.USER.email) = LOWER(:email)", {"email": username})
+            result = cursor.fetchone()
+
+            if result:
+                if passh == result[0]:
+                    # Username and password are valid, so set user as logged in
+                    session['username'] = username
+                    session['userrole'] = result[5]
+                    session['userid'] = result[1]
+                    return redirect(url_for('index'))
+                else:
+                    err = 'Invalid username or password'
+            
+        
+     #   return render_template('login.html',err=err) # redundant
+    
+    return render_template('login.html',err=err)
+
 
 if __name__ == '__main__':
     app.run()
