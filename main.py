cf2f527c2041a04a40d6cdd1c44875d89f56cd7e
pardisnorouzi <22223614@studentmail.ul.ie>
2023-05-13 11:40:22 +0100

    updating with main

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import datetime
 from flask import Flask , session, redirect, url_for, request, render_template
-#import oracledb
+import oracledb
+import hashlib
 
 # Replace with your actual Oracle database credentials
 user = 'SYS'
 password = 'root'
 port = 1522
 service_name = 'XEPDB1'
-server = 'NAS'
-conn_string = "server:{port}/{service_name}".format(
+conn_string = "localhost:{port}/{service_name}".format(
     port=port, service_name=service_name)
 app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
 data = []
 id = []
 
-@app.route('/')
+@app.route('/homepage')
+def home():
+    return render_template('homepage.html')
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
+@app.route('/')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
