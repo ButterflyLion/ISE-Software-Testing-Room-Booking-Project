7e1cafc9d3bb76af9bb2d27ae46f4fc1b878eb3c
pardisnorouzi <22223614@studentmail.ul.ie>
2023-05-13 11:17:47 +0100

    db connection to localhost

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import datetime
 from flask import Flask , session, redirect, url_for, request, render_template
-#import oracledb
+import oracledb
+import hashlib
 
 # Replace with your actual Oracle database credentials
 user = 'SYS'
 password = 'root'
 port = 1522
 service_name = 'XEPDB1'
-server = 'NAS'
-conn_string = "server:{port}/{service_name}".format(
+conn_string = "localhost:{port}/{service_name}".format(
     port=port, service_name=service_name)
 app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
 data = []
