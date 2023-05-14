4ebb946da69a0c9e2e7e331275369ed0266f41be
conorglynn02 <conorglynn02@gmail.com>
2023-05-14 19:45:46 +0100

    changed unittest file slightly

--- unit_test.py
+++ unit_test.py
@@ -6,37 +6,44 @@ class TestRoomBookingSystem(unittest.TestCase):
         #return super().setUp()
         self.booking_system = RoomBookingSystem()
 
+    
+    #date tests
+    #insert date tests
+
+    
+    #time tests
+    
     def can_book_random_good_time(self):
-        self.assertTrue(booking_system.book_room('user1', '2', 'room1', '17-05-2023', '14'))
+        self.assertTrue(booking_system.book_room('1@studentmail.ul.ie', '1', 'room1', '17-05-2023', '14'))
 
     def can_book_l_limit_good_time(self):
-        self.assertTrue(booking_system.book_room('user1', '2', 'room1', '17-05-2023', '8'))
+        self.assertTrue(booking_system.book_room('1@studentmail.ul.ie', '1', 'room1', '17-05-2023', '8'))
 
     def can_book_u_limit_good_time(self):
-        self.assertTrue(booking_system.book_room('user1', '2', 'room1', '17-05-2023', '20'))
+        self.assertTrue(booking_system.book_room('1@studentmail.ul.ie', '1', 'room1', '17-05-2023', '20'))
 
 
     def cannot_book_random_bad_time(self):
-        self.assertFalse(booking_system.book_room('user1', '2', 'room1', '17-05-2023', '2'))
+        self.assertFalse(booking_system.book_room('1@studentmail.ul.ie', '1', 'room1', '17-05-2023', '2'))
 
     def can_book_l_limit_bad_time(self):
-        self.assertFalse(booking_system.book_room('user1', '2', 'room1', '17-05-2023', '21'))
+        self.assertFalse(booking_system.book_room('1@studentmail.ul.ie', '1', 'room1', '17-05-2023', '21'))
 
     def can_book_u_limit_bad_time(self):
-        self.assertFalse(booking_system.book_room('user1', '2', 'room1', '17-05-2023', '7'))
+        self.assertFalse(booking_system.book_room('1@studentmail.ul.ie', '1', 'room1', '17-05-2023', '7'))
         
         
     def cannot_book_same_time(self):
-        self.booking_system.book_room('user1', '2', 'room1', '17-05-2023', '14')
+        self.booking_system.book_room('1@studentmail.ul.ie', '1', 'room1', '17-05-2023', '14')
 
-        self.assertFalse(booking_system.book_room('user1', '2', 'room1', '17-05-2023', '14'))
+        self.assertFalse(booking_system.book_room('1@studentmail.ul.ie', '1', 'room1', '17-05-2023', '14'))
 
     def can_book_right_before_time(self):
-        self.booking_system.book_room('user1', '2', 'room1', '17-05-2023', '14')
+        self.booking_system.book_room('1@studentmail.ul.ie', '1', 'room1', '17-05-2023', '14')
 
-        self.assertTrue(booking_system.book_room('user1', '2', 'room1', '17-05-2023', '13'))
+        self.assertTrue(booking_system.book_room('1@studentmail.ul.ie', '1', 'room1', '17-05-2023', '13'))
 
     def can_book_right_after_time(self):
-        self.booking_system.book_room('user1', '2', 'room1', '17-05-2023', '14')
+        self.booking_system.book_room('1@studentmail.ul.ie', '1', 'room1', '17-05-2023', '14')
 
-        self.assertTrue(booking_system.book_room('user1', '2', 'room1', '17-05-2023', '15'))
+        self.assertTrue(booking_system.book_room('1@studentmail.ul.ie', '1', 'room1', '17-05-2023', '15'))
