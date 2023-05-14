ab9444381c3156f54bd7499052ba6a94b68b6465
ButterflyLion <orosztama@gmail.com>
2023-05-14 22:42:35 +0100

    first draft

--- functionalTests/NewBooking.py
+++ functionalTests/NewBooking.py
@@ -9,29 +9,45 @@ browser = Browser('chrome', **executable_path, headless=False, options=options)
 url = 'http://127.0.0.1:5000/index'
 browser.visit(url)
 
-if(browser.is_element_present_by_name("submit")):
-    browser.find_by_name("submit").click()
-    if(browser.is_element_present_by_id("guests")):
-        browser.find_by_id("guests").fill(5)
+browser.find_by_name("username").fill("6@studentmail.ul.ie")
+browser.find_by_name("password").fill(12345)
+login = browser.find_by_value("Log In").click()
+
+if(browser.url == "http://127.0.0.1:5000/index"):
+    if(browser.is_element_present_by_name("submit")):
         browser.find_by_name("submit").click()
+        if(browser.is_element_present_by_id("guests")):
+            browser.find_by_id("guests").fill(5)
+            browser.find_by_name("submit").click()
 
-        if(browser.is_element_present_by_id("tbl")):
-            headers = ["Room Description", "Min Capacity", "Max Capacity", "Book This"]
-            for header in headers:
-                assert browser.is_text_present(header), f"Header '{header}' not found"
-                browser.find_by_id("tbl")
-                browser.find_by_name('Book').first.click()
-                
+            if(browser.is_element_present_by_id("tbl")):
+                headers = ["Room Description", "Min Capacity", "Max Capacity", "Book This"]
+                for header in headers:
+                    assert browser.is_text_present(header), f"Header '{header}' not found"
+                button = browser.find_by_css('.btn.bookbtn')
+                button.first.click()
+                if(browser.is_element_present_by_css("#datepicker")):
+                    browser.find_by_text("25").click()  #selects 25th of May
+                    browser.find_by_name("submit").click()
+                    if(browser.is_element_present_by_css(".btn.hourbtn")):
+                        browser.find_by_text("20:00 - 21:00").click()  
 
+                    else:
+                        assert browser.is_element_present_by_css(".btn.hourbtn"), "There are no time slots to pick from"            
 
+                else:
+                    assert browser.is_element_present_by_css("#datepicker"), "Calendar doesn't exist"
+
+            else:
+                assert browser.is_element_present_by_id("tbl"), "Table not found for choosing a room to book"
 
         else:
-            assert browser.is_element_present_by_id("tbl"), "Table not found"
+            assert browser.is_element_present_by_id("guests"), "Input field for specifying no. of guests doesn't exist"
 
     else:
-        assert browser.is_element_present_by_id("guests"), "Input field for specifying no. of guests doesn't exist"
+        assert browser.is_element_present_by_name("submit"), "No 'Make a Booking' button exists"
 
 else:
-    assert browser.is_element_present_by_name("submit"), "No 'Make a Booking' button exists"
+    print("Login failed")
 
 browser.quit()
