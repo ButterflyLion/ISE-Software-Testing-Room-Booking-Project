b3203489ef197f02b92e30db571319a432bdd93b
pardisnorouzi <22223614@studentmail.ul.ie>
2023-05-15 15:06:12 +0100

    Updated_System_Test

--- System_Test.py
+++ System_Test.py
@@ -4,106 +4,153 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 import time
 
-# launch the browser and navigate to the login page
-driver = webdriver.Chrome()
-driver.get("http://127.0.0.1:5000/index")
+# Code for the happy path scenario
+class TestHappyPath:
+    def test_login_and_make_booking(self):
+        # launch the browser and navigate to the login page
+        driver = webdriver.Chrome()
+        driver.get("http://127.0.0.1:5000/index")
 
-# enter email and password and click login
-email_field = driver.find_element("name", "username")
-email_field.send_keys("1@studentmail.ul.ie")
+        # enter email and password and click login
+        email_field = driver.find_element("name", "username")
+        email_field.send_keys("1@studentmail.ul.ie")
 
-password_field = driver.find_element("name", "password")
-password_field.send_keys("12345")
-password_field.send_keys(Keys.RETURN)
+        password_field = driver.find_element("name", "password")
+        password_field.send_keys("12345")
+        password_field.send_keys(Keys.RETURN)
 
-# wait for the system to load and verify that the user has successfully logged in
-time.sleep(2)  # wait for 5 seconds to let the page load
-assert "Welcome" in driver.page_source
+        # wait for the system to load and verify that the user has successfully logged in
+        time.sleep(2)  # wait for 5 seconds to let the page load
+        assert "Welcome" in driver.page_source
 
-# navigate to the next page and click on the Make a Booking button
-driver.find_element("name" , "submit").click()
+        # navigate to the next page and click on the Make a Booking button
+        driver.find_element("name" , "submit").click()
 
-# navigate to the booking page and enter the number of guests
-guests_field = driver.find_element("name" , "guests")
-guests_field.send_keys("1")
-guests_field.send_keys(Keys.RETURN)
-#submit_button = driver.find_element("name", "submit").click()
+        # navigate to the booking page and enter the number of guests
+        guests_field = driver.find_element("name" , "guests")
+        guests_field.send_keys("1")
+        guests_field.send_keys(Keys.RETURN)
+        #submit_button = driver.find_element("name", "submit").click()
 
-# wait for the system to load and verify that the booking was successful
-time.sleep(2)  # wait for 5 seconds to let the page load
-assert "Available Rooms for" in driver.page_source
+        # wait for the system to load and verify that the booking was successful
+        time.sleep(2)  # wait for 5 seconds to let the page load
+        assert "Available Rooms for" in driver.page_source
 
-driver.find_element("id" , "2").click()
-
-
-# wait for the system to load and verify that the booking was successful
-time.sleep(2)  # wait for 5 seconds to let the page load
-assert "Available Dates for" in driver.page_source
-
-# Locate the datepicker input element and click on it
-datepicker = driver.find_element("id", "datepicker")
-datepicker.click()
+        driver.find_element("id" , "2").click()
 
 
-# Click on the datepicker input element again to close the widget (if necessary)
-datepicker.click()
+        # wait for the system to load and verify that the booking was successful
+        time.sleep(2)  # wait for 5 seconds to let the page load
+        assert "Available Dates for" in driver.page_source
 
-# Select the desired date in the datepicker widget
-date_element = driver.find_element(By.XPATH, "//a[@data-date='25']")
-date_element.click()
-
-driver.find_element("name" , "submit").click()
-
-time.sleep(3) 
-
-driver.find_element(By.CSS_SELECTOR, ".hourbtn:first-of-type").click()
-
-time.sleep(3)
-
-driver.find_element("name" , "submit").click()
-
-time.sleep(3)
-
-driver.find_element("name" , "submit").click()
-
-time.sleep(10)
+        # Locate the datepicker input element and click on it
+        datepicker = driver.find_element("id", "datepicker")
+        datepicker.click()
 
 
+        # Click on the datepicker input element again to close the widget (if necessary)
+        datepicker.click()
 
-# # verify that the system shows a list of available rooms with a capacity of at least 1 guest
-# time.sleep(5)  # wait for 5 seconds to let the page load
-# room_list = driver.find_element_by_id("room-list")
-# assert "BooK" in room_list.text
+        # Select the desired date in the datepicker widget
+        date_element = driver.find_element(By.XPATH, "//a[@data-date='25']")
+        date_element.click()
 
-# # select the Study Room from the list of available rooms
-# study_room_button = driver.find_element_by_id("Submit")
-# study_room_button.click()
+        driver.find_element("name" , "submit").click()
 
-# # choose the date 05/17/2023 from the calendar
-# date_picker = driver.find_element_by_id("date-picker")
-# date_picker.click()
-# select = Select(driver.find_element_by_class_name("ui-datepicker-year"))
-# select.select_by_value("2023")
-# select = Select(driver.find_element_by_class_name("ui-datepicker-month"))
-# select.select_by_value("5")
-# driver.find_element_by_xpath("//a[text()='17']").click()
+        time.sleep(3) 
 
-# # verify that the system displays available time slots for the chosen date
-# time.sleep(5)  # wait for 5 seconds to let the page load
-# time_slot_list = driver.find_element_by_id("time-slot-list")
-# assert "8:00 AM - 9:00 AM" in time_slot_list.text
+        driver.find_element(By.CSS_SELECTOR, ".hourbtn:first-of-type").click()
 
-# # choose the first available time slot from 8:00 AM to 9:00 AM
-# time_slot_button = driver.find_element_by_id("time-slot-button-0")
-# time_slot_button.click()
+        time.sleep(3)
 
-# # click on the Confirm button to book the Study Room for the chosen time slot
-# confirm_button = driver.find_element_by_id("confirm-button")
-# confirm_button.click()
+        driver.find_element("name" , "submit").click()
 
-# # verify that the system displays a confirmation message for the booking
-# time.sleep(5)  # wait for 5 seconds to let the page load
-# assert "confirm" in driver.page_source
+        time.sleep(3)
 
-# close the browser
-driver.quit()
+        driver.find_element("name" , "submit").click()
+
+        time.sleep(10)
+
+        driver.quit()
+
+
+# Test that the same person can not book another room at the same date and time        
+class TestBookingSameRoomTimeAndDate:
+    def test_booking_without_information(self):
+        # launch the browser and navigate to the login page
+        driver = webdriver.Chrome()
+        driver.get("http://127.0.0.1:5000/index")
+
+        # enter email and password and click login
+        email_field = driver.find_element("name", "username")
+        email_field.send_keys("1@studentmail.ul.ie")
+
+        password_field = driver.find_element("name", "password")
+        password_field.send_keys("12345")
+        password_field.send_keys(Keys.RETURN)      
+
+        # wait for the system to load and verify that the user has successfully logged in
+        time.sleep(2)  # wait for 5 seconds to let the page load
+        assert "Welcome" in driver.page_source
+    
+    # navigate to the next page and click on the Make a Booking button
+        driver.find_element("name" , "submit").click()
+
+        # navigate to the booking page and enter the number of guests
+        guests_field = driver.find_element("name" , "guests")
+        guests_field.send_keys("1")
+        guests_field.send_keys(Keys.RETURN)
+        #submit_button = driver.find_element("name", "submit").click()
+
+        # wait for the system to load and verify that the booking was successful
+        time.sleep(2)  # wait for 5 seconds to let the page load
+        assert "Available Rooms for" in driver.page_source
+
+        driver.find_element("id" , "1").click()
+
+
+        # wait for the system to load and verify that the booking was successful
+        time.sleep(2)  # wait for 5 seconds to let the page load
+        assert "Available Dates for" in driver.page_source
+
+        # Locate the datepicker input element and click on it
+        datepicker = driver.find_element("id", "datepicker")
+        datepicker.click()
+
+
+        # Click on the datepicker input element again to close the widget (if necessary)
+        datepicker.click()
+
+        # Select the desired date in the datepicker widget
+        date_element = driver.find_element(By.XPATH, "//a[@data-date='25']")
+        date_element.click()
+
+        driver.find_element("name" , "submit").click()
+
+        time.sleep(3) 
+
+        driver.find_element(By.CSS_SELECTOR, ".hourbtn:first-of-type").click()
+
+        time.sleep(3)
+
+        driver.find_element("name" , "submit").click()
+
+        time.sleep(3)
+
+        driver.find_element("name" , "submit").click()
+
+        time.sleep(10)
+
+        driver.quit()
+
+# Test that the same person can not book more than two times a day 
+#class TestBookingMoreThanTwiceADay:
+    
+
+
+# Test that the person can not book a full room
+
+
+# Test concurrent booking with threads
+
+
